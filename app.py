d877740698c5a53bfbca38fbe524a6825560f32a
SHUBHANKAR MAITY <indiavivo956@gmail.com>
2026-06-07 21:04:42 +0530

    updated code

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def convert_to_village_style(text):
         'ঠিক': 'ঠিক',
         'দরকার': 'লাগবো',
         'কেমন': 'কিমান',
-        # More village mapping words/phrases you can add here
+        # More village mapping words/phrases you can add 
     }
     for std, vill in mapping.items():
         text = text.replace(std, vill)
